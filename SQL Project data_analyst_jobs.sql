03bb14f2cc3c8075911e1923ca44bb9e3cf3ba9b
Johnathan Van Dusen <jvd97@icloud.com>
2025-07-30 18:48:34 -0500

    July 30 Push

--- SQL Project data_analyst_jobs.sql
+++ SQL Project data_analyst_jobs.sql
@@ -2,7 +2,7 @@
 --1.    How many rows are in the data_analyst_jobs table? 
 --Answer: 1793
 
-SELECT *
+SELECT COUNT (*)
 FROM data_analyst_jobs;
 
 --2.	Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row? 
@@ -27,21 +27,22 @@ WHERE location = 'TN' OR location = 'KY';
 --4.	How many postings in Tennessee have a star rating above 4?
 --Answer: 3
 
-SELECT location, star_rating
+SELECT COUNT (*) 
 FROM data_analyst_jobs
 WHERE star_rating > 4 AND location = 'TN';
 
 --5.	How many postings in the dataset have a review count between 500 and 1000?
 --Answer: 151
 
-SELECT COUNT(review_count)
+SELECT COUNT(*)
 FROM data_analyst_jobs
 WHERE review_count BETWEEN 500 AND 1000;
 
 --6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
 
-SELECT location AS state, AVG(star_rating) AS avg_rating
+SELECT location AS state, ROUND(AVG(star_rating), 2) AS avg_rating
 FROM data_analyst_jobs
+WHERE location IS NOT NULL AND star_rating IS NOT NULL
 GROUP BY location
 ORDER BY avg_rating DESC;
 
@@ -61,10 +62,11 @@ WHERE location = 'CA';
 
 --9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
 
-SELECT company AS company, ROUND(AVG(star_rating),2) AS avg_rating
+SELECT company, ROUND(AVG(star_rating),2) AS avg_rating
 FROM data_analyst_jobs
-WHERE review_count > 5000
+WHERE review_count > 5000 AND company IS NOT NULL
 GROUP BY company
+ORDER BY avg_rating DESC
 
 
 SELECT COUNT(company) AS company_count
@@ -85,9 +87,9 @@ ORDER BY avg_rating DESC;
 --11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
 --Answer 1636
 
-SELECT COUNT(title)
+SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs
-WHERE title LIKE '%Analyst%'
+WHERE title ILIKE '%analyst%'
 
 --12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
 --Answer: 4
@@ -102,7 +104,7 @@ WHERE title NOT ILIKE '%Analyst%' AND title NOT ILIKE '%Analytics%'
  -- Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
  -- Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4?
 
- SELECT domain, COUNT(title) AS count_titles
+ SELECT domain AS industry, COUNT(title) AS count_titles
  FROM data_analyst_jobs
  WHERE days_since_posting > 21
  	AND skill ILIKE '%SQL%'
